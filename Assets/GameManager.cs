b86527093b918a148f726fb09d1fe4698312b7d2
Fazal Ashraf <fazalashrafofficial@gmail.com>
2023-10-05 15:15:15 +0530

    intial push

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Canvas pauseMenuCanvas;
+    private bool IsPaused = false;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+
+            if (IsPaused) {
+                Debug.Log("Resume Game");
+                ResumeGame();
+            } else {
+                Debug.Log("Pause Game");
+                PauseGame();
+            }
+
+
+            void PauseGame() {
+                IsPaused = true;
+                Time.timeScale = 0; // Pause the game
+               // pauseMenuCanvas.enabled = true; // Show the pause menu UI
+            }
+
+            void ResumeGame() {
+                IsPaused = false;
+                Time.timeScale = 1; // Resume the game
+                //pauseMenuCanvas.enabled = false; // Hide the pause menu UI
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,299 @@
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Camera Tracking")]
+    [SerializeField] private GameObject _cameraFollowGO;
+
+    //public Animator animator;
+    private float horizontal;
+    [SerializeField]private float speed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
+    public bool isFacingRight = true;
+
+    private bool canDash = true;
+    private bool isDashing;
+    [SerializeField] private float dashingPower = 24f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+    private float dashDirection;
+
+    private bool isWallsliding;
+    private float wallSlidingSpeed = 2f;
+
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    //private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration = 0.4f;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
+    private CameraFollowObject _cameraFollowObject;
+    private float _fallSpeedYDampingChangeThreshold;
+
+    private bool isFalling = false; // Track if the player is falling
+
+
+    private bool isJumping;
+
+    [SerializeField] private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    [SerializeField] private float jumpBufferTime = 0.2f;
+    private float jumpBufferCounter;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+
+
+    private void Start() {
+        Cursor.visible = false;
+        _cameraFollowObject = _cameraFollowGO.GetComponent<CameraFollowObject>();
+
+        _fallSpeedYDampingChangeThreshold = CameraManager.instance._fallSpeedYDampingChangeThreshold;
+    }
+
+
+    private void Update() {
+        if (isDashing) {
+            return;
+        }
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+
+        //animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        if (IsGrounded()) {
+            coyoteTimeCounter = coyoteTime;
+        } else {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump")) {
+            jumpBufferCounter = jumpBufferTime;
+        } else {
+            jumpBufferCounter -= Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
+        }
+
+        if (coyoteTimeCounter > 0f && jumpBufferCounter > 0f && !isJumping) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
+            jumpBufferCounter = 0f;
+
+            StartCoroutine(JumpCooldown());
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f) {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+
+            coyoteTimeCounter = 0f;
+        }
+
+        WallSlide();
+        WallJump();
+
+        if(!isWallJumping) {
+       //// Flip();
+       TurnCheck();
+
+        }
+        
+        //Debug.Log(transform.rotation.y);
+
+        ////if we are falling past a certain speed threshold
+        //if(rb.velocity.y < _fallSpeedYDampingChangeThreshold && !CameraManager.instance.IsLerpingYDamping && !CameraManager.instance.LerpedFromPlayerFalling) {
+        //    CameraManager.instance.LerpYDamping(true);
+        //}
+
+        ////if we are standing or moving up
+        //if(rb.velocity.y >= 0f && !CameraManager.instance.IsLerpingYDamping && !CameraManager.instance.LerpedFromPlayerFalling) {
+        //    //reset so it can be called again
+        //    CameraManager.instance.LerpedFromPlayerFalling = false;
+
+        //    CameraManager.instance.LerpYDamping(false);
+        //}
+
+        // Check if the player is falling and notify CameraManager to change YDamping
+        if (rb.velocity.y < _fallSpeedYDampingChangeThreshold && !isFalling) {
+            isFalling = true;
+            StartFalling(); // Call this when the player starts falling
+        } else if (rb.velocity.y >= 0f && isFalling) {
+            isFalling = false;
+            StartStandingOrMoving(); // Call this when the player is standing or moving
+        }
+    }
+
+    private void FixedUpdate() {
+        if (isDashing) {
+            return;
+        }
+        
+        if(horizontal > 0f || horizontal < 0f) {
+
+        TurnCheck() ;
+        }
+
+
+        if (!isWallJumping) {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);         
+        }
+    }
+
+    private bool IsGrounded() {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private bool IsWalled() {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide() {
+        if(IsWalled() && !IsGrounded() && horizontal != 0f) {
+            isWallsliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        } else {
+            isWallsliding = false;
+        }
+    }
+
+    //private void WallJump() {
+    //    if (isWallsliding) {
+    //        isWallJumping = false;
+    //        wallJumpingDirection = -transform.rota;
+    //        wallJumpingCounter = wallJumpingTime;
+
+    //        CancelInvoke(nameof(StopWallJumping));
+    //    } else {
+    //        wallJumpingCounter -= Time.deltaTime;
+    //    }
+
+    //    if(Input.GetButtonDown("Jump") && wallJumpingCounter > 0f) { 
+    //        isWallJumping = true;
+    //        rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+    //        wallJumpingCounter = 0f;
+
+    //        //if(transform.localScale.x != wallJumpingDirection) {
+    //        //    isFacingRight = !isFacingRight;
+    //        //    Vector3 localScale = transform.localScale;
+    //        //    localScale.x *= -1f;
+    //        //    transform.localScale = localScale;
+    //        //}
+
+
+
+    //        Invoke(nameof(StopWallJumping), wallJumpingDuration);
+    //    }
+    //}
+
+    // When the player starts falling, call this function to set Y damping to 0.25f
+    void StartFalling() {
+        CameraManager.instance.ChangeYDamping(0.25f);
+    }
+
+    // When the player is standing or moving horizontally, call this function to set Y damping to 2.0f
+    void StartStandingOrMoving() {
+        CameraManager.instance.ChangeYDamping(2.0f);
+    }
+
+    private void WallJump() {
+        if (isWallsliding && Input.GetButtonDown("Jump")) {
+            wallJumpingDirection = isFacingRight ? -1f : 1f; // Determine the direction of the wall jump
+            rb.velocity = new Vector2(wallJumpingPower.x * wallJumpingDirection, wallJumpingPower.y);
+            isWallJumping = true;
+            wallJumpingCounter = wallJumpingDuration;
+
+            // Rotate the character to face the direction of the wall jump
+            Vector3 newRotation = transform.rotation.eulerAngles;
+            newRotation.y = isFacingRight ? 0f : 180f;
+            transform.rotation = Quaternion.Euler(newRotation);
+        }
+
+        if (isWallJumping) {
+            wallJumpingCounter -= Time.deltaTime;
+
+            // Check if the wall jump duration has ended
+            if (wallJumpingCounter <= 0f) {
+                isWallJumping = false;
+            }
+        }
+    }
+
+
+
+    private void StopWallJumping() {
+        isWallJumping = false;
+    }
+
+    //private void Flip() {
+    //    if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) {
+    //        Vector3 localScale = transform.localScale;
+    //        isFacingRight = !isFacingRight;
+    //        localScale.x *= -1f;
+    //        transform.localScale = localScale;
+    //    }
+    //}
+
+    private IEnumerator JumpCooldown() {
+        isJumping = true;
+        yield return new WaitForSeconds(0.4f);
+        isJumping = false;
+    }
+
+    private IEnumerator Dash() {
+
+        canDash = false;
+        isDashing = true;
+
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0;
+        
+        dashDirection = isFacingRight ? 1f : -1f;
+        rb.velocity = new Vector2(dashDirection * dashingPower, 0f);
+        trailRenderer.emitting = true;
+
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+    private void TurnCheck() {
+        if(horizontal > 0f && !isFacingRight) {
+            Turn();
+        }else if(horizontal < 0f && isFacingRight) {
+            Turn();
+        }
+    }
+
+    private void Turn() {
+        if(isFacingRight) {
+
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+
+            _cameraFollowObject.CallTurn();
+        } else {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = !isFacingRight;
+
+            _cameraFollowObject.CallTurn();
+        }
+    }
+}
